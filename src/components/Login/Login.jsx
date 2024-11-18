26b2495223a2b7124d815c708f91cbaa32b399f4
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-11-18 21:31:18 +0600

    validate email address and forget password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from "react";
 import { auth } from "../../firebase.init";
 import { Link } from "react-router-dom";
 
@@ -7,6 +7,7 @@ const Login = () => {
 
     const [success, setSuccess] = useState(false);
     const [loginError, setLoginError] = useState('');
+    const emailRef = useRef();
 
 const handleLogin = e =>{
     e.preventDefault();
@@ -26,7 +27,15 @@ const handleLogin = e =>{
     signInWithEmailAndPassword(auth, email, password)
     .then(result => {
         console.log(result.user);
-        setSuccess(true);
+
+        if(!result.user.emailVerified){
+            setLoginError('Please verify your email address.')
+        }
+        else{          
+            setSuccess(true);
+        }
+        
+        
     })
     .catch(error => {
         console.log('ERROR', error.message);
@@ -34,6 +43,22 @@ const handleLogin = e =>{
     })
 
 }
+
+const handleForgetPassword = () => {
+    console.log('get me email address', emailRef.current.value);
+    const email = emailRef.current.value;
+    if(!email){
+       console.log('Please provide a valid email address.')
+    }
+    else {
+        sendPasswordResetEmail(auth, email)
+        .then(() =>{
+            alert('Password Reset email sent, please check your email');
+        })
+    }
+}
+
+
    
   return (
     <div className="hero bg-base-200 min-h-screen">
@@ -55,6 +80,7 @@ const handleLogin = e =>{
               <input
                 type="email"
                 name="email"
+                ref={emailRef}
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -71,7 +97,7 @@ const handleLogin = e =>{
                 className="input input-bordered"
                 required
               />
-              <label className="label">
+              <label onClick={handleForgetPassword} className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
                 </a>
